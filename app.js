054d1d143e63ed2c51d89c8e13ce902ca780c33d
coderblower <saifulrmail@gmail.com>
2020-09-15 15:28:55 +0600

    masterss4448

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let express = require('express');
-
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb+srv://saiful:55tFUcugqZ5Du56d@cluster0.vxj27.mongodb.net/<dbname>?retryWrites=true&w=majority'
 let app = express();
 
-app.get('/', (req, res)=>{
-    res.send('hello')
+const client = new MongoClient(uri);
+
+app.get('/', async (req, res)=>{
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+        res.send('success ')
+ 
+    } catch (e) {
+        res.send('error')
+    } finally {
+        await client.close();
+    }
 })
 
 let env = process.env.PORT || 3000; 
